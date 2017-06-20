33cfe0ed067a1fd6efe871fb75110096b457b558
Andrew Tonner <rakslice@gmail.com>
2017-06-20 02:17:12 -0700

    update for brew QTDIR

--- distfiles/deploy_mac.sh
+++ distfiles/deploy_mac.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e -x
 
-QTVER=5.9
+QTVER=5.9.0
+
+QTDIR=/usr/local/Cellar/qt/$QTVER
 
 # macdeploy
-~/Qt/$QTVER/clang_64/bin/macdeployqt orion.app -qmldir=~/git/orion/src/qml
+$QTDIR/clang_64/bin/macdeployqt orion.app -qmldir=~/git/orion/src/qml
 
 # qml libs 
 mkdir orion.app/Contents/Resources/qml
-#cp -r ~/Qt/$QTVER/clang_64/qml/Enginio orion.app/Contents/Resources/qml
-cp -r ~/Qt/$QTVER/clang_64/qml/Qt orion.app/Contents/Resources/qml
-cp -r ~/Qt/$QTVER/clang_64/qml/QtQml orion.app/Contents/Resources/qml
-cp -r ~/Qt/$QTVER/clang_64/qml/QtQuick orion.app/Contents/Resources/qml
-cp -r ~/Qt/$QTVER/clang_64/qml/QtQuick.2 orion.app/Contents/Resources/qml
-#cp -r ~/Qt/$QTVER/clang_64/qml/Communi orion.app/Contents/Resources/qml
+#cp -r $QTDIR/clang_64/qml/Enginio orion.app/Contents/Resources/qml
+cp -r $QTDIR/clang_64/qml/Qt orion.app/Contents/Resources/qml
+cp -r $QTDIR/clang_64/qml/QtQml orion.app/Contents/Resources/qml
+cp -r $QTDIR/clang_64/qml/QtQuick orion.app/Contents/Resources/qml
+cp -r $QTDIR/clang_64/qml/QtQuick.2 orion.app/Contents/Resources/qml
+#cp -r $QTDIR/clang_64/qml/Communi orion.app/Contents/Resources/qml
 
 #sh fixlibs.sh orion.app
 
