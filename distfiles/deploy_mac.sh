21c55411e81250825c72085f71955c0a4efa74a3
Andrew Tonner <rakslice@gmail.com>
2017-06-20 04:09:14 -0700

    more deploy paths tweaks

--- distfiles/deploy_mac.sh
+++ distfiles/deploy_mac.sh
@@ -6,16 +6,16 @@ QTVER=5.9.0
 QTDIR=/usr/local/Cellar/qt/$QTVER
 
 # macdeploy
-$QTDIR/clang_64/bin/macdeployqt orion.app -qmldir=~/git/orion/src/qml
+$QTDIR/bin/macdeployqt orion.app -qmldir=./src/qml
 
 # qml libs 
 mkdir orion.app/Contents/Resources/qml
-#cp -r $QTDIR/clang_64/qml/Enginio orion.app/Contents/Resources/qml
-cp -r $QTDIR/clang_64/qml/Qt orion.app/Contents/Resources/qml
-cp -r $QTDIR/clang_64/qml/QtQml orion.app/Contents/Resources/qml
-cp -r $QTDIR/clang_64/qml/QtQuick orion.app/Contents/Resources/qml
-cp -r $QTDIR/clang_64/qml/QtQuick.2 orion.app/Contents/Resources/qml
-#cp -r $QTDIR/clang_64/qml/Communi orion.app/Contents/Resources/qml
+#cp -r $QTDIR/qml/Enginio orion.app/Contents/Resources/qml
+cp -r $QTDIR/qml/Qt orion.app/Contents/Resources/qml
+cp -r $QTDIR/qml/QtQml orion.app/Contents/Resources/qml
+cp -r $QTDIR/qml/QtQuick orion.app/Contents/Resources/qml
+cp -r $QTDIR/qml/QtQuick.2 orion.app/Contents/Resources/qml
+#cp -r $QTDIR/qml/Communi orion.app/Contents/Resources/qml
 
 #sh fixlibs.sh orion.app
 
