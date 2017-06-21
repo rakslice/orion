bb145b1c615014601fbe988569c1121666d46bf9
Andrew Tonner <rakslice@gmail.com>
2017-06-21 14:22:52 -0700

    cleanup

--- distfiles/deploy_mac.sh
+++ distfiles/deploy_mac.sh
@@ -6,12 +6,10 @@ $QTDIR/bin/macdeployqt orion.app -qmldir=./src/qml
 
 # qml libs 
 mkdir -p orion.app/Contents/Resources/qml
-#cp -r $QTDIR/qml/Enginio orion.app/Contents/Resources/qml
 cp -r $QTDIR/qml/Qt orion.app/Contents/Resources/qml
 cp -r $QTDIR/qml/QtQml orion.app/Contents/Resources/qml
 cp -r $QTDIR/qml/QtQuick orion.app/Contents/Resources/qml
 cp -r $QTDIR/qml/QtQuick.2 orion.app/Contents/Resources/qml
-#cp -r $QTDIR/qml/Communi orion.app/Contents/Resources/qml
 
 sh distfiles/fixlibs.sh orion.app
 
