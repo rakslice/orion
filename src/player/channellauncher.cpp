f3b71dc2df1ffc59c32553b2934ebc93e0e8c04e
rakslice <rakslice@gmail.com>
2020-01-02 23:30:48 -0800

    make external launcher more general purpose and cross platform; remove hardcode

--- src/player/channellauncher.cpp
+++ src/player/channellauncher.cpp
@@ -1,14 +1,29 @@
 #include "channellauncher.h"
 
 #include <QProcess>
+#include <QStandardPaths>
+#include <QDebug>
 
 
 Q_INVOKABLE void ChannelLauncher::launchChannel(QVariant channelId) {
+
     QStringList args;
+    QString prog;
+    QString script = QStandardPaths::standardLocations(QStandardPaths::HomeLocation).first() + "/open_twitch_stream";
+
+#ifdef Q_OS_WIN32
+    prog = "cmd.exe";
     args << "/C";
-    args << "C:\\Users\\Andrew Tonner\\Documents\\kodicontrol\\kodicontrol.cmd";
+    script += ".cmd";
+#else
+    prog = "bash";
+    args << "-e";
+#endif
+
+    qDebug() << "Using external script at" << script;
+    args << script;
     args << channelId.toString();
 
     QProcess process(this);
-    process.startDetached("cmd.exe", args);
+    process.startDetached(prog, args);
 }
